5fd79ad1d80fc2c21ec25e66e0a72ebd3bc651b8
Nicolas Casamen <sebastian.casamen@udla.edu.ec>
2024-04-24 10:59:59 -0500

    Creacion de la clase cine con sus respectivos atributos

---
+++ Models/NC_Cine.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NicolasCasamen_Examen1P.Models
+{
+    public class NC_Cine
+    {
+        [Required]
+        public int NC_NumeroBoleto { get; set; }
+        [Required]
+        public String? NC_Pelicula { get; set; }
+        [Required]
+        public DateTime NC_FechaPelicula { get; set; }
+        [Required]
+        public int NC_Entradas { get; set; }
+        [Required]
+        public bool NC_Socio { get; set; }
+        [VerificarSocio]
+        public decimal NC_Costo { get; set; }
+    }
+    public class VerificarSocio : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            decimal valor = (decimal)value;
+            if (valor < 15)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
